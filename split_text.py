bab310a3979ebb5434b05a79d164f81a1ef23d02
Kiran <kgawas9@gmail.com>
2023-03-09 16:20:22 +0530

    removed un-necessary code

--- split_text.py
+++ split_text.py
@@ -1,19 +1,19 @@
-import re
+"""
+Test use case to split the documents based on certain condition
+"""
 
-# test_string = '2-dimethylaminoethanol, 2-[(2-[2-(dimethylamino)ethoxy]ethyl)methylamino]ethanol'
+test_string = '2-dimethylaminoethanol, 2-[(2-[2-(dimethylamino)ethoxy]ethyl)methylamino]ethanol'
 # test_string = 'METHANOL,KETHANOL, 1,2-DICHLOROBENZENE'
 # test_string = '1,3,5-TRIS(2-HYDROXYETHYL) HEXAHYDRO-S-TRIAZINE, 1,2-DICHLOROBENZENE,METHANOL,KETHANOL, 1,2-DICHLOROBENZENE'
 # test_string = 'Nickle Charoride (NiCl2), Hydrochloric acid'
 # test_string = 'D009, D008'
-test_string = 'Light aromatic naphtha, 1,2,3-Trimmethylbenzene'
+# test_string = 'Light aromatic naphtha, 1,2,3-Trimmethylbenzene'
+
 
 splitted_list = test_string.split(',')
-
 chemical_lst = []
-tmp_lst = []
-
 str_chemical_val = ''
-temp_val = False
+
 
 for item in splitted_list:
     # print('current item : ', item , ' | previous item : ', tmp_lst[-1])
@@ -30,8 +30,6 @@ for item in splitted_list:
         else:
             str_chemical_val = item.strip()
         # print('current value : ', item, ' | chemical string val : ', str_chemical_val)
-
-    tmp_lst.append(item)
 
 print(chemical_lst)
 
