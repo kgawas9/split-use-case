cede329d64a538e1290e33b246e97bf7f48dbe47
Kiran <kgawas9@gmail.com>
2023-03-09 22:14:19 +0530

    split text based on regex

--- split_text.py
+++ split_text.py
@@ -1,68 +1,50 @@
 """
 Test use case to split the documents based on certain condition
 """
+import re
 
 test_string = '2-dimethylaminoethanol, 2-[(2-[2-(dimethylamino)ethoxy]ethyl)methylamino]ethanol'
-# test_string = 'METHANOL,KETHANOL, 1,2-DICHLOROBENZENE'
-# test_string = '1,3,5-TRIS(2-HYDROXYETHYL) HEXAHYDRO-S-TRIAZINE, 1,2-DICHLOROBENZENE,METHANOL,KETHANOL, 1,2-DICHLOROBENZENE'
-# test_string = 'Nickle Charoride (NiCl2), Hydrochloric acid'
-# test_string = 'D009, D008'
-# test_string = 'Light aromatic naphtha, 1,2,3-Trimmethylbenzene'
+test_string = 'METHANOL,KETHANOL, 1,2-DICHLOROBENZENE'
+test_string = '1,3,5-TRIS(2-HYDROXYETHYL) HEXAHYDRO-S-TRIAZINE, 1,2-DICHLOROBENZENE,METHANOL,KETHANOL, 1,2-DICHLOROBENZENE'
+test_string = 'Nickle Charoride (NiCl2), Hydrochloric acid'
+test_string = 'D009, D008'
+test_string = 'Light aromatic naphtha, 1,2,3-Trimmethylbenzene'
+test_string = 'ACETON, PEROFLUORD-3,6,9-TrOXATRIDEANOIC ACID'
+test_string = '1,3,5-Triazine-1,3,5(2H,H,6H)-triethanol, 1,2-Benzisothiazolin-3-one'
+
+split_text = re.split(r"\b\w{3}[\W_]\w{1,2},", test_string)
+print(split_text)
+
+# splitted_list = test_string.split(',')
+# chemical_lst = []
+# str_chemical_val = ''
 
 
-splitted_list = test_string.split(',')
-chemical_lst = []
-str_chemical_val = ''
+# for item in splitted_list:
 
-
-for item in splitted_list:
-    # print('current item : ', item , ' | previous item : ', tmp_lst[-1])
-    
-    if len(item) > 3:
-            if str_chemical_val:
-                 chemical_lst.append(str_chemical_val + "," + item.strip())
-                 str_chemical_val=''
-            else:
-                chemical_lst.append(item.strip())
-    else:
-        if str_chemical_val:
-             str_chemical_val = str_chemical_val + "," + item.strip()
-        else:
-            str_chemical_val = item.strip()
-        # print('current value : ', item, ' | chemical string val : ', str_chemical_val)
-
-print(chemical_lst)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#     try:
-#         if tmp_lst[-1][-3]:
-#             # print('in try - if')
+#     if len(item) > 3:
 #             if str_chemical_val:
-#                 print('in str_chemical value')
-#                 chemical_lst.append(str_chemical_val)
-#                 str_chemical_val = ''
-#             chemical_lst.append(item)
-#     except:
-#         print('in except')
-#         str_chemical_val =  str_chemical_val + "," + item
-#         print('previous value ', [tmp_lst[-1]], ' | current value : ', item, ' | chemical string val : ', str_chemical_val)
+#                  chemical_lst.append(str_chemical_val + "," + item.strip())
+#                  str_chemical_val=''
+#             else:
+#                 if item[0] == ' ':
+#                     if str_chemical_val:
+#                         str_chemical_val = str_chemical_val + "," + item.strip()
+#                     else:
+#                         str_chemical_val = item.strip()
+#                         if not len(splitted_list) > 2:
+#                             chemical_lst.append(str_chemical_val)
+#                             str_chemical_val=''
+#                 else:
+#                     chemical_lst.append(item.strip())
+#     else:
 
-#     tmp_lst.append(item)
+#         if str_chemical_val:
+#              str_chemical_val = str_chemical_val + "," + item.strip()
+#         else:
+#             str_chemical_val = item.strip()
+#         # print('current value : ', item, ' | chemical string val : ', str_chemical_val)
 
 # print(chemical_lst)
+
+
